181365dddca9ab9d8620b1c8979057a4fb8bff52
carambola <celestebaltazar@yahoo.com>
2023-02-28 15:52:35 -0800

    set up projects sections

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import DotGroup from "./scenes/DotGroup";
 import Landing from "./scenes/Landing";
 import LineGradient from "./components/LineGradient";
 import MySkills from "./scenes/MySkills";
-
+import Projects from "./scenes/Projects";
 function App() {
   const [selectedPage, setSelectedPage] = useState('home');
   const [isTopOfPage, setIsTopOfPage] = useState(true);
@@ -37,6 +37,9 @@ function App() {
     <div className="w-5/6 mx-auto md:h-full">
       <MySkills />
     </div>
+    <div className="w-5/6 mx-auto">
+      <Projects />
+      </div>
   </div>
 }
 
