4a13ed96b5cc839c390dc00b8805de99cccb33af
carambola <celestebaltazar@yahoo.com>
2023-04-06 21:58:09 -0700

    added space between btn

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -59,7 +59,7 @@ const Landing = ({ setSelectedPage }) => {
                 </motion.div>
                 {/* call to action */}
                 <motion.div
-                    className="flex mt-5 justify-center md:justify-start"
+                    className="space-x-1 flex mt-5 justify-center md:justify-start"
                     initial="hidden"
                     whileInView="visible"
                     viewport={{ once: true, amount: 0.5 }}
