0cbaafc35bd1be6a28f712432f3328a889236291
carambola <celestebaltazar@yahoo.com>
2023-04-06 21:28:43 -0700

    changed bg and btn color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        lbeige: "#FFFFFF",
-        blue: "#E26D5C",
-        "dbeige": "#C9CBA3",
-        ruby: "#723D46",
-        grey: "#FFE1A8",
-        "f-blue": "#E26D5C",
+        lbeige: "#FAF9F6",
+        blue: "#03045e", /* dark blue */
+        "dbeige": "#0077b6",/* lighter blue */
+        ruby: "#000000",
+        grey: "#0077b6",
+        "f-blue": "#03045e",/* dark blue */
         "opaque-black": "rgba(0,0,0,0.35)",
         text: "#472D30",
         linkin: "#6da7e6",
         lruby: "#f08080",
-        must: "#C9CBA3"
+        must:  "#0077b6"/* lighter blue */
         
       },
       backgroundImage: (theme) => ({
@@ -23,7 +23,7 @@ module.exports = {
           "linear-gradient(81.66deg, #c9def4 7.21%, #f5ccd4 45.05%, #b8a4c9 78.07%)",
 
         "gradient-rainblue":
-          "linear-gradient(90deg, #EF6351 14.53%, #FFE3E0 117.73%)",
+          "linear-gradient(90deg, #000000 14.53%, #0096c7 117.73%)",
       }),
       fontFamily: {
         playfair: ["monospace", "raleway"],
